c4dcbc8b4a208718ad7c7ae7f70f2a8afb07635f
Micaela <micapareddes@gmail.com>
2022-08-04 19:17:07 -0300

    base game

--- game.js
+++ game.js
@@ -1,11 +1,37 @@
 console.log('FlappyBird');
 
 const sprites = new Image();
-sprites.src = './sprites.png'
+sprites.src = './img/sprites.png'
 
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
+//-flappy bird---------
+const flappyBird = {
+    sourceX: 0,
+    sourceY: 0,
+    width: 34,
+    height: 24,
+    x: (canvas.width / 2) - 100 / 2,
+    y: 180,
+    gravity: 0.25,
+    velocity: 0,
+    refresh() {
+        flappyBird.velocity = flappyBird.velocity + flappyBird.gravity; /* para que a velocidade aumente conforme o flappy bird desce */
+        flappyBird.y = flappyBird.y + flappyBird.velocity; /* para que caia */
+    },
+    draw() {
+        context.drawImage(
+            sprites, /* image= imagem que vamos usar, nesse caso sprites */
+            flappyBird.sourceX, flappyBird.sourceY, /* sprite X, sprite Y /* pedaço da imagem, sprites, que vamos pegar */
+            flappyBird.width, flappyBird.height, /* tamanho do recorte na sprite */
+            flappyBird.x, flappyBird.y, /* tamanho do x e y no canvas*/
+            flappyBird.width, flappyBird.height, /* tamanho do width e height no canvas */
+        );
+    },
+};
+
+//-landscape------------
 const background = {
     sourceX: 390,
     sourceY: 0,
@@ -54,44 +80,79 @@ const floor = {
     },
 };
 
-const flappyBird = {
-    sourceX: 0,
-    sourceY: 0,
-    width: 34,
-    height: 24,
-    x: 10,
-    y: 50,
-    gravity: 0.25,
-    velocity: 0,
-    refresh() {
-        flappyBird.velocity = flappyBird.velocity + flappyBird.gravity; /* para que a velocidade aumente conforme o flappy bird desce */
-        flappyBird.y = flappyBird.y + flappyBird.velocity; /* para que caia */
+//-screens----------
+let activeScreen = {};
+function changeScreen(newScreen){
+    activeScreen = newScreen;
+}
+
+
+const screens = {
+    getReady: {
+        draw() {
+            background.draw();
+        
+            floor.draw();
+        
+            flappyBird.draw();
+
+            getReady.draw();
+            
+        },
+        click(){
+            changeScreen(screens.gamePlay);
+        },
+        refresh() {},
     },
-    draw() {
-        context.drawImage(
-            sprites, /* image= imagem que vamos usar, nesse caso sprites */
-            flappyBird.sourceX, flappyBird.sourceY, /* sprite X, sprite Y /* pedaço da imagem, sprites, que vamos pegar */
-            flappyBird.width, flappyBird.height, /* tamanho do recorte na sprite */
-            flappyBird.x, flappyBird.y, /* tamanho do x e y no canvas*/
-            flappyBird.width, flappyBird.height, /* tamanho do width e height no canvas */
-        );
+
+    gamePlay: {
+        draw(){
+
+            background.draw();
+        
+            floor.draw();
+        
+            flappyBird.draw();
+        },
+        refresh() {
+            flappyBird.refresh();
+        },
     },
 };
 
-/* um jogo desenha as coisas varias vezes 'Frames Per Second', para nos ajudar com isso usamos o requestAnimationFrame
-= desenha os quadros na tela da forma mais inteligente possivel */
+const getReady = {
+    sX: 134,
+    sY: 0,
+    w: 174,
+    h: 152,
+    x: (canvas.width / 2) - 172 / 2,
+    y: 100,
+    draw() {
+        context.drawImage(
+            sprites,
+            getReady.sX, getReady.sY,
+            getReady.w, getReady.h,
+            getReady.x, getReady.y,
+            getReady.w, getReady.h,
+        );
+
+    },
+};
+
+
 function loop() {/* ODEM DE PRIORIDADE É LIDA DE BAIXO PRA CIMA */
+    activeScreen.draw();
+    activeScreen.refresh();
 
-    flappyBird.refresh();
-
-    background.draw();
-
-    floor.draw();
-
-    flappyBird.draw();
-
-    requestAnimationFrame(loop);
+    requestAnimationFrame(loop);/* um jogo desenha as coisas varias vezes 'Frames Per Second', para nos ajudar com isso usamos o requestAnimationFrame = desenha os quadros na tela da forma mais inteligente possivel */
 
 }
 
+window.addEventListener('click', function (){
+    if (activeScreen.click){
+        activeScreen.click();
+    }
+});
+
+changeScreen(screens.getReady);
 loop();
